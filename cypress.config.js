326aba3f03fb1a74a691c965fc3ca7e47aef6ca9
Alexandre DERET <[allex.deret@gmail.com](mailto:allex.deret@gmail.com)>
2023-05-11 09:40:18 +0200

    Generated a HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'yvsawm',
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl : 'https://www.demoblaze.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on)
     },
   },
 });
